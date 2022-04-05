06a4963c95118a7a5fa9ab1b2acaaff76735adc4
LisandroCausa <lisandro.anexa@gmail.com>
2022-04-05 15:46:55 -0300

    Create practica1_7.pas (parte 2)

---
+++ 1er semestre/CADP/Practicas/Practica 1/parte2/practica1_7.pas
@@ -0,0 +1,62 @@
+program siete;
+var
+	piloto	: string;
+	tiempo	: real;
+	i		: integer;
+
+	pilotoPrimerPuesto	: string;
+	tiempoPrimerPuesto	: real;
+
+	pilotoSegundoPuesto	: string;
+	tiempoSegundoPuesto	: real;
+
+	pilotoAnteultimoPuesto	: string;
+	tiempoAnteultimoPuesto	: real;
+
+	pilotoUltimoPuesto	: string;
+	tiempoUltimoPuesto	: real;
+begin
+	tiempoPrimerPuesto := 9999;
+	tiempoSegundoPuesto := 9999;
+	tiempoUltimoPuesto := -9999;
+	tiempoAnteultimoPuesto := -9999;
+	for i:=1 to 10 do
+	begin
+		write('Piloto: ');
+		readln(piloto);
+		write('Tiempo: ');
+		readln(tiempo);
+		writeln('---------------------');
+		if(tiempo < tiempoPrimerPuesto) then
+		begin
+			pilotoSegundoPuesto := pilotoPrimerPuesto;
+			pilotoPrimerPuesto := piloto;
+
+			tiempoSegundoPuesto := tiempoPrimerPuesto;
+			tiempoPrimerPuesto := tiempo;
+		end
+		else if(tiempo < tiempoSegundoPuesto) then
+		begin
+			pilotoSegundoPuesto := piloto;
+			tiempoSegundoPuesto := tiempo;
+		end;
+
+		if(tiempo > tiempoUltimoPuesto) then
+		begin
+			pilotoAnteultimoPuesto := pilotoUltimoPuesto;
+			pilotoUltimoPuesto := piloto;
+
+			tiempoAnteultimoPuesto := tiempoUltimoPuesto;
+			tiempoUltimoPuesto := tiempo;
+		end
+		else if(tiempo > tiempoAnteultimoPuesto) then
+		begin
+			pilotoAnteultimoPuesto := piloto;
+			tiempoAnteultimoPuesto := tiempo;
+		end;
+	end;
+	writeln('Primer puesto: ', pilotoPrimerPuesto);
+	writeln('Segundo puesto: ', pilotoSegundoPuesto);
+	writeln('Anteultimo puesto: ', pilotoAnteultimoPuesto);
+	writeln('Ultimo puesto: ', pilotoUltimoPuesto);
+end.

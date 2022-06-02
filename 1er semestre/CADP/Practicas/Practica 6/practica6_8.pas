dbcf3936ec9119b41fa0801795d545d71fa06b7b
LisandroCausa <lisandro.anexa@gmail.com>
2022-06-01 21:05:35 -0300

    Add practica6_8.pas

---
+++ 1er semestre/CADP/Practicas/Practica 6/practica6_8.pas
@@ -0,0 +1,18 @@
+program ocho;
+type
+	lista = ^nodo;
+	nodo = record
+		num : integer;
+		sig : lista;
+	end;
+
+procedure armarNodo(var L: lista; v: integer);
+var
+	aux : lista;
+begin
+	new(aux);
+	aux^.num := v;
+	aux^.sig := nil;
+	L^.sig := aux;
+	L := L^.sig;
+end;

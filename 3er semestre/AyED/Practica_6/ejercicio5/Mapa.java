0750094ae39a7c65ad9d27729dc47cb185124f56
Lisandro Causa <lisandro.anexa@gmail.com>
2023-06-11 12:17:33 -0300

    Add ejercicio 5 practica 6 AyED

---
+++ 3er semestre/AyED/Practica_6/ejercicio5/Mapa.java
@@ -0,0 +1,227 @@
+package ejercicio5;
+
+import tp02.ejercicio2.*;
+import tp02.ejercicio3.*;
+import tp06.ejercicio3.*;
+
+public class Mapa {
+	private Grafo<String> mapa;
+	
+	public Mapa(Grafo<String> mapa) {
+		this.mapa = mapa;
+	}
+	
+	public ListaGenerica<String> devolverCamino(String ciudad1, String ciudad2) {
+		ListaGenerica<String> camino = new ListaEnlazadaGenerica<String>();
+		
+		Vertice<String> verticeCiudad1 = buscarVertice(ciudad1);
+		
+		boolean[] visitados = new boolean[this.mapa.listaDeVertices().tamanio()];
+		
+		primerDFS(verticeCiudad1.getPosicion(), camino, ciudad2, visitados);
+		
+		return camino;
+	}
+	
+	private Vertice<String> buscarVertice(String dato) {
+		ListaGenerica<Vertice<String>> vertices = this.mapa.listaDeVertices();
+		vertices.comenzar();
+		while(!vertices.fin()) {
+			Vertice<String> proximo = vertices.proximo();
+			if(proximo.dato().equals(dato)) {
+				return proximo;
+			}
+		}
+		return null;
+	}
+	
+	private boolean primerDFS(int p, ListaGenerica<String> camino, String buscado, boolean[] visitados) {
+		boolean encontrado = false;
+		Vertice<String> vertice = this.mapa.vertice(p);
+		if(visitados[vertice.getPosicion()])
+			return false;
+		visitados[vertice.getPosicion()] = true;
+			
+		if(vertice.dato().equals(buscado)) {
+			encontrado = true;
+		} else {
+			ListaGenerica<Arista<String>> adyacentes = this.mapa.listaDeAdyacentes(vertice);
+			adyacentes.comenzar();
+			while(!adyacentes.fin() && !encontrado) {
+				Arista<String> proximo = adyacentes.proximo();
+				encontrado = primerDFS(proximo.verticeDestino().getPosicion(), camino, buscado, visitados);
+			}
+		}
+		if(encontrado)
+			camino.agregarInicio(vertice.dato());
+		return encontrado;
+	}
+	
+	public ListaGenerica<String> devolverCaminoExceptuando(String ciudad1, String ciudad2, ListaGenerica<String> ciudades) {
+		ListaGenerica<String> camino = new ListaEnlazadaGenerica<String>();
+		
+		Vertice<String> verticeCiudad1 = buscarVertice(ciudad1);
+		
+		boolean[] visitados = new boolean[this.mapa.listaDeVertices().tamanio()];
+		
+		exceptuandoDFS(verticeCiudad1.getPosicion(), camino, ciudad2, ciudades, visitados);
+		
+		return camino;
+	}
+	
+	private boolean exceptuandoDFS(int p, ListaGenerica<String> camino, String buscado, ListaGenerica<String> excepciones, boolean[] visitados) {
+		boolean encontrado = false;
+		Vertice<String> vertice = this.mapa.vertice(p);
+		if(visitados[vertice.getPosicion()])
+			return false;
+		visitados[vertice.getPosicion()] = true;
+		if(vertice.dato().equals(buscado)) {
+			encontrado = true;
+		} else {
+			excepciones.comenzar();
+			while(!excepciones.fin()) {
+				if(vertice.dato().equals(excepciones.proximo())) {
+					return false;
+				}
+			}
+			ListaGenerica<Arista<String>> adyacentes = this.mapa.listaDeAdyacentes(vertice);
+			adyacentes.comenzar();
+			while(!adyacentes.fin() && !encontrado) {
+				Arista<String> proximo = adyacentes.proximo();
+				encontrado = exceptuandoDFS(proximo.verticeDestino().getPosicion(), camino, buscado, excepciones, visitados);
+			}
+		}
+		if(encontrado)
+			camino.agregarInicio(vertice.dato());
+		return encontrado;
+	}
+	
+	public ListaGenerica<String> caminoMasCorto(String ciudad1, String ciudad2) {
+		Vertice<String> verticeOrigen = buscarVertice(ciudad1);
+		Vertice<String> verticeDestino = buscarVertice(ciudad2);
+		
+		int n = this.mapa.listaDeVertices().tamanio();
+		int[] D = new int[n+1];
+		int[] P = new int [n+1];
+		DijkstraAciclico(D, P, verticeOrigen.getPosicion());
+		
+		ListaGenerica<String> camino = new ListaEnlazadaGenerica<String>();
+		int i = verticeDestino.getPosicion();
+		while(P[i] != 0) {
+			camino.agregarInicio(this.mapa.vertice(i).dato());
+			i = P[i];
+		}
+		camino.agregarInicio(this.mapa.vertice(i).dato());
+		
+		return camino;
+	}
+	
+	private void Dijkstra(int[] D, int[] P, int origen) {
+		int n = this.mapa.listaDeVertices().tamanio();
+		boolean[] visitado = new boolean[n+1];
+		
+		for(int i = 1; i <= n; i++) {
+			D[i] = 99999;
+			P[i] = 0;
+		}
+		D[origen] = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			int minIndice = -1;
+			int minD = Integer.MAX_VALUE;
+			for(int j = 1; j <= n; j++) {
+				if(visitado[j] == false && D[j] < minD) {
+					minIndice = j;
+					minD = D[minIndice];
+				}
+			}
+			visitado[minIndice] = true;
+			Vertice<String> vertice = this.mapa.vertice(minIndice);
+			ListaGenerica<Arista<String>> adyacentes = this.mapa.listaDeAdyacentes(vertice);
+			adyacentes.comenzar();
+			while(!adyacentes.fin()) {
+				Arista<String> arista = adyacentes.proximo();
+				int proximo = arista.verticeDestino().getPosicion();
+				if(visitado[proximo] == false) {
+					if(D[proximo] > D[minIndice] + arista.peso()) {
+						D[proximo] = D[minIndice] + arista.peso();
+						P[proximo] = minIndice;
+					}
+				}
+			}
+		}
+	}
+	
+	private int[] grados_in() {
+		int n = this.mapa.listaDeVertices().tamanio();
+		int[] array = new int[n+1];
+		for(int i = 1; i <= n; i++) {
+			ListaGenerica<Arista<String>> adyacentes = this.mapa.listaDeAdyacentes(this.mapa.vertice(i));
+			adyacentes.comenzar();
+			while(!adyacentes.fin()) {
+				array[adyacentes.proximo().verticeDestino().getPosicion()] += 1;
+			}
+		}
+		return array;
+	}
+	
+	private void DijkstraAciclico(int[] D, int[] P, int origen) {
+		int n = this.mapa.listaDeVertices().tamanio();
+		int[] in = grados_in();
+		ColaGenerica<Vertice<String>> cola = new ColaGenerica<Vertice<String>>();
+		for(int i = 1; i <= n; i++) {
+			if(in[i] == 0) {
+				cola.encolar(this.mapa.vertice(i));
+			}
+			D[i] = 9999;
+			P[i] = 0;
+		}
+		D[origen] = 0;
+		while(!cola.esVacia()) {
+			Vertice<String> proximo = cola.desencolar();
+			ListaGenerica<Arista<String>> adyacentes = this.mapa.listaDeAdyacentes(proximo);
+			adyacentes.comenzar();
+			while(!adyacentes.fin()) {
+				Arista<String> arista = adyacentes.proximo();
+				int w = arista.verticeDestino().getPosicion();
+				in[w] -= 1;
+				if(in[w] == 0) {
+					cola.encolar(this.mapa.vertice(w));
+				}
+				if(D[proximo.getPosicion()] != 9999) {
+					if(D[w] > D[proximo.getPosicion()] + arista.peso()) {
+						D[w] = D[proximo.getPosicion()] + arista.peso();
+						P[w] = proximo.getPosicion();
+					}
+				}
+			}
+		}
+	}
+	
+	public ListaGenerica<String> caminoSinCargarCombustible(String ciudad1, String ciudad2, int tanqueAuto) {
+		Vertice<String> verticeOrigen = buscarVertice(ciudad1);
+		Vertice<String> verticeDestino = buscarVertice(ciudad2);
+		
+		int n = this.mapa.listaDeVertices().tamanio();
+		int[] D = new int[n+1];
+		int[] P = new int[n+1];
+		DijkstraAciclico(D, P, verticeOrigen.getPosicion());
+		
+		ListaGenerica<String> camino = new ListaEnlazadaGenerica<String>();
+		int i = verticeDestino.getPosicion();
+		if(D[i] <= tanqueAuto) {
+			while(i != 0) {
+				camino.agregarInicio(this.mapa.vertice(i).dato());
+				i = P[i];
+			}
+			if(tanqueAuto < 0) {
+				while(!camino.esVacia()) {
+					camino.eliminarEn(1);
+				}
+			}
+		}
+
+		
+		return camino;
+	}
+}
---
+++ 3er semestre/AyED/Practica_6/ejercicio5/TestMapa.java
@@ -0,0 +1,74 @@
+package ejercicio5;
+
+import tp02.ejercicio2.*;
+import tp06.ejercicio3.*;
+
+public class TestMapa {
+
+	// creditos test: https://github.com/agusrnfr/AyED/blob/main/Practicas/src/tp06/ejercicio5/MapaTest.java
+	public static void main(String[] args) {
+		Vertice<String> v1 = new VerticeImplListAdy<String>("Buenos Aires");
+		Vertice<String> v2 = new VerticeImplListAdy<String>("Santiago");
+		Vertice<String> v3 = new VerticeImplListAdy<String>("Lima");
+		Vertice<String> v4 = new VerticeImplListAdy<String>("Montevideo");
+		Vertice<String> v5 = new VerticeImplListAdy<String>("Asuncion");
+		Vertice<String> v6 = new VerticeImplListAdy<String>("Caracas");
+		Vertice<String> v7 = new VerticeImplListAdy<String>("La Habana");
+
+		Grafo<String> ciudades = new GrafoImplListAdy<String>();
+		
+		ciudades.agregarVertice(v1);
+		ciudades.agregarVertice(v2);
+		ciudades.agregarVertice(v3);
+		ciudades.agregarVertice(v4);
+		ciudades.agregarVertice(v5);
+		ciudades.agregarVertice(v6);
+		ciudades.agregarVertice(v7);
+
+		ciudades.conectar(v1, v2,3);
+		ciudades.conectar(v1, v3,2);
+		ciudades.conectar(v1, v4,4);
+		ciudades.conectar(v1, v5,10);
+		ciudades.conectar(v2, v5,500);
+		ciudades.conectar(v2, v7,4);
+		ciudades.conectar(v3, v5,4);
+		ciudades.conectar(v4, v5,6);
+		ciudades.conectar(v6, v5,11);
+		ciudades.conectar(v6, v7,10);
+		
+		Mapa mapa = new Mapa (ciudades);
+
+		ListaGenerica<String> lis = mapa.devolverCamino("Buenos Aires","Asuncion");
+		System.out.println("--- Se imprime devolverCamino ---");
+		lis.comenzar();
+		while(!lis.fin()) {
+			System.out.println(lis.proximo());
+		}
+		
+		ListaGenerica<String> exceptuando = new ListaEnlazadaGenerica<String>();
+		exceptuando.agregarFinal("Santiago");
+		exceptuando.agregarFinal("Lima");
+		exceptuando.agregarFinal("Montevideo");
+		ListaGenerica<String> lis2 = mapa.devolverCaminoExceptuando("Buenos Aires","Asuncion",exceptuando);
+		System.out.println("--- Se imprime devolverCaminoExceptuando ---");
+		lis2.comenzar();
+		while(!lis2.fin()) {
+			System.out.println(lis2.proximo());
+		}
+		
+		ListaGenerica<String> lis3 = mapa.caminoMasCorto("Buenos Aires","Asuncion");
+		System.out.println("--- Se imprime caminoMasCorto ---");
+		lis3.comenzar();
+		while(!lis3.fin()) {
+			System.out.println(lis3.proximo());
+		}
+		
+		ListaGenerica<String> lis4 = mapa.caminoSinCargarCombustible("Buenos Aires", "Asuncion", 7);
+		System.out.println("--- Se imprime caminoSinCargarCombustible ---");
+		lis4.comenzar();
+		while(!lis4.fin()) {
+			System.out.println(lis4.proximo());
+		}
+	}
+
+}
